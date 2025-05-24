5bb88bb78e5e64fbfa98b12968294964ecfd59c0
Avei20 <muhammadabdulazizalghofari@gmail.com>
2025-05-25 03:04:37 +0700

    adjust config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    preview: {
+      allowedHosts: ["*"],
+    },
+    server: {
+      host: "0.0.0.0",
+    },
+  },
 });
